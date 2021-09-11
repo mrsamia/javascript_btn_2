6fbe5c4fd10b244f684eef843abdfcbe75aa06de
mrsamia <15203060@iubat.edu>
2021-09-11 03:37:17 -0700

    SRM | first_commit

---
+++ script.js
@@ -0,0 +1,56 @@
+function myFunction() {
+    var img = document.getElementById("id1");
+    if(img.innerHTML=="Message"){
+    img.innerHTML = "Sent";
+    img.style.color='white';
+    img.style.backgroundColor = "#064DDE";
+    
+   }
+   else if(img.innerHTML=="Sent"){
+   img.innerHTML = "Message";
+   img.style.color='black';
+   img.style.backgroundColor = "#FF7A7A";
+  
+   }
+   
+   }
+
+
+   function myFunctions() {
+    var img = document.getElementById("id2");
+    if(img.innerHTML=="Subscribe"){
+    img.innerHTML = "Subscribed";
+    img.style.color='white';
+    img.style.backgroundColor = "#CC0000";
+    
+   }
+   else if(img.innerHTML=="Subscribed"){
+   img.innerHTML = "Subscribe";
+   img.style.color='black';
+   img.style.backgroundColor = "#FF7A7A";
+  
+   }
+   
+   }
+  
+
+   function myFunctionss() {
+    var img = document.getElementById("id3");
+    if(img.innerHTML=="Follow"){
+    img.innerHTML = "Unfollow";
+    img.style.color='white';
+    img.style.backgroundColor = "#DA7700";
+    
+   }
+   else if(img.innerHTML=="Unfollow"){
+   img.innerHTML = "Follow";
+   img.style.color='black';
+   img.style.backgroundColor = "#FF7A7A";
+  
+   }
+   
+   }
+  
+  
+  
+ 
